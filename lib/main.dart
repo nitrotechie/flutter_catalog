619391e2b367d80abc38da15a6b4dec798b38760
nitrotechie <s.ankit1140@gmail.com>
2021-05-13 13:08:43 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text('Welcome'),
+          ),
+        ),
+      ),
+    );
+  }
+}
