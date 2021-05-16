afe804c6583d067bc318eda3ff247121f645af22
Ankit Singh <s.ankit1140@gmail.com>
2021-05-16 11:46:04 +0530

    Day 22 Completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,14 +1,96 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/widget/themes.dart';
 
 class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: MyTheme.creamColor,
+      backgroundColor: Theme.of(context).canvasColor,
       appBar: AppBar(
         backgroundColor: Theme.of(context).canvasColor,
         title: Text("Cart"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: _CartList(),
+            ),
+          ),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            "Total: \$900",
+            style: TextStyle(
+              color: Theme.of(context).accentColor,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(
+            width: 100,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                "Buy Now",
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            ),
+            style: ButtonStyle(
+                shape: MaterialStateProperty.all(StadiumBorder()),
+                backgroundColor: MaterialStateProperty.all(
+                  Theme.of(context).buttonColor,
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  @override
+  __CartListState createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          icon: Icon(CupertinoIcons.delete),
+          onPressed: () {},
+        ),
+        title: Text(
+          "Item 1",
+          style: TextStyle(
+            color: Theme.of(context).accentColor,
+          ),
+        ),
       ),
     );
   }
